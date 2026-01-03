12b134523b5aff6af677d82fc4804632d2d08cd7
Rishith Reddy Raghu <rishith.chintu@gmail.com>
2026-01-03 19:19:50 +0530

    Adding attention.py

---
+++ attention.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+    
+# Step 1: Compute Query, Key, Value
+def compute_Q_K_V():
+    # X is the output of the embedding layer (plus positional encoding) for a sequence.
+    # Shape of x --> n * d_model, where n is number of tokens in the sequence and d_model is the embedding model size 
+    # W's are learned weight matrices that linearly project token embeddings into query, key, value
+    # Shape of W's are d_model * d_k, where d_model is the embedding model size and d_k = d_model for single head for multi head i.e for n heads d_k becomes d_model / n
+    # For example embedding model size is 512 and 8 heads, then d_k will be 512/8 = 64
+    # Shape of Q, K, V is n * d_k
+    # Q is Query, what the token is looking for in other tokens. Role: Used to compare against all keys
+    # K is Key, what this token offers to other tokens. Role: Compared withe queries to determine relevance
+    # V is Value, what information this token actually contributes. Role: Weighted and summed to form the output
+    Q = X @ W_Q
+    K = X @ W_K
+    V = X @ W_V
+    return Q, K, V
+
+# Step 2; Compute Similarity score, it's for finding relevant tokens. Higher score means more relevant
+def simiilarity():
+    # Since the shape of Q and K is n * d_k, we are going to perform transpose on K, so we can perfrom matrix multiplication on Q and K
+    # Shape of score will be n * n
+    score = Q @ K.T
+    return score
+
+# Step 3: Scaling for similarity score
+def scale():
+    # Since the dot products grow as the d_k i.e emdedding model size increases, and it grows by sqrt of d_k
+    score = score / np.sqrt(d_k)
+
+# Step 4: Computing Softmax from scaled similarity score
+def softmax():
+    # softmax turns the scaled similarity scores into probablities. Takes a vector of real numbers i.e Scaled similarity scores ans tuens them into all positive numbers
+    # All outputs will be between 0 and 1
+    softmax = np.exp(S)/np.sum(np.exp(S), axis = 1)
+    return softmax
+
+def compute_attention():
+    output = softmax @ V 
